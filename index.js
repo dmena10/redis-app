614371a31a24cfaf77ef4d59b1e6258a0db180ea
Daniel Mena <dmenabarreto@gmail.com>
2025-06-18 16:44:05 -0300

    arquivos do nodejs

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const redis = require('redis');
+
+const app =  express();
+const client = redis.createClient({host: 'redis-server',
+    port: 6379});
+
+
+client.set('vissits', 0);
+
+app.get('/', (req, res) => {
+  client.get('vissits', (err, visits) => {
+    if (err) {
+      console.error('Error retrieving visits:', err);
+      return res.status(500).send('Internal Server Error');
+    }
+    visits = parseInt(visits, 10) + 1;
+    client.set('vissits', visits);
+    res.send(`Number of visits: ${visits}`);
+  });
+});
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
