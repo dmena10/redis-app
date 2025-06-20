14191cf2437f868f79d31880769ff05312026ef4
Daniel Mena <dmenabarreto@gmail.com>
2025-06-20 13:55:58 -0300

    nova correção4

--- teste-app.sh
+++ teste-app.sh
@@ -1,7 +1,13 @@
 RESULT="'wget -qO - http://localhost:8080/ '"
 wget -q localhost:8090
-if [ $? -eq 0 ]; then
+if [ $? -eq 0 ] 
+then
     echo "Redis app is running successfully."
+elif [[ $RESULT == *"Number"*]]
+then
+    echo "ok - number of visists"
+    echo $RESULT
 else
     echo "Redis app is not running."
+    exit 1
 fi
